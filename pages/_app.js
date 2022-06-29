9e4cfcf823effa596efa632e7a13d9d610528a67
Alexey <a.generalov92@outlook.com>
2022-06-29 12:07:58 +0300

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react"
+import { MoralisProvider } from "react-moralis";
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <MoralisProvider appId={process.env.NEXT_PUBLIC_APPID} serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}>
         <Component {...pageProps} />
+      </MoralisProvider>
     </ChakraProvider>
   ) 
 }
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,44 @@
-export default function Home() {
-  return(
-    <div>
-      <h1>
-        some text
-      </h1>
-    </div>
-  )
-}}
+import { Button, Flex, Text} from "@chakra-ui/react"
+import Head from "next/head"
+import { useMoralis } from "react-moralis"
 
-export default index;
+export default function Home() {
+  const {isAuthenticated, authenticate} = useMoralis()
+
+  if(!isAuthenticated) {
+    return(
+      <>
+      <Head>
+        <title>Login | EtherTarcker</title>
+      </Head>
+      <Flex
+        direction="column"
+        justifyContent="center" 
+        alignItems="center"
+        width="100vw"
+        height="100vh"
+        bgGradient="linear(to-br, yellow.400, pink.300)">
+          <Text 
+            fontSize="5xl" 
+            fontWeight="bold" 
+            color="white">
+            EtherTracker</Text>
+          <Button 
+            colorScheme="orange"
+            size="lg"
+            mt="6"
+            onClick={() => authenticate()}>
+            Login with Metamask</Button>
+      </Flex>
+      </>
+    )
+  }
+  return(
+    <Flex
+    height="100vh"
+    weight="100vw"
+    bgGradient="linear(to-br, teal.400, yellow.400)">
+
+    </Flex>
+  )
+}
