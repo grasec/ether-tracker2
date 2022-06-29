ea7d6291ef2b9889681f6d36c0a10a19af5239c4
Alexey <a.generalov92@outlook.com>
2022-06-29 16:28:47 +0300

    tx

---
+++ components/Transactions.js
@@ -0,0 +1,9 @@
+import MyContainer from "./myContainer";
+
+export default function Transactions(){
+    return(
+        <MyContainer>
+            some text
+        </MyContainer>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-import { Button, Flex, Text} from "@chakra-ui/react"
+import { Box, Button, Flex, Tab, TabList, TabPanel, TabPanels, Tabs, Text} from "@chakra-ui/react"
 import Head from "next/head"
 import { useMoralis } from "react-moralis"
+import Header from "../components/Header"
+import Profile from "../components/Profile"
+import Transactions from "../components/Transactions"
 
 export default function Home() {
-  const {isAuthenticated, authenticate} = useMoralis()
+  const {isAuthenticated, authenticate, user, logout, isLoggingOut} = useMoralis()
 
   if(!isAuthenticated) {
     return(
@@ -27,18 +30,44 @@ export default function Home() {
             colorScheme="orange"
             size="lg"
             mt="6"
-            onClick={() => authenticate()}>
+            onClick={() => authenticate({
+              signingMessage: "Sign to Login to EtherTracker"
+            })}>
             Login with Metamask</Button>
       </Flex>
       </>
     )
   }
   return(
+    <>
+    <Head>
+      <title> EtherTracker </title>
+    </Head>
     <Flex
-    height="100vh"
-    weight="100vw"
-    bgGradient="linear(to-br, teal.400, yellow.400)">
-
+      direction="column"
+      height="100vh"
+      weight="100vw"
+      >
+        <Header user={user} logout={logout} isLoggingOut={isLoggingOut}/>
+        <Box flex="1" px="40" py="20"
+        bgGradient="linear(to-br, teal.400, yellow.400)">
+          <Tabs size="lg" colorScheme="yellow"
+          align="center" variant="enclosed">
+            <TabList>
+              <Tab fontWeight="bold">Profile</Tab>
+              <Tab fontWeight="bold">Transactions</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <Profile user={user}/>
+              </TabPanel>
+              <TabPanel>
+                <Transactions />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
     </Flex>
+    </>
   )
 }
